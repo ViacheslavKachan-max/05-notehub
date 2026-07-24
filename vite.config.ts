0fd3e11fad24f47cd8bea08095e2524305bd5ffc
Slavik <vaceslavkacan@gmail.com>
2026-07-24 20:16:12 +0200

    remove-Vercel/githab-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  // GitHub Pages serves project sites from /<repo-name>/
-  base: command === "build" ? "/05-notehub/" : "/",
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-}));
+export default defineConfig({
+  base: process.env.VERCEL ? "/" : "/05-notehub/",
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
+});
